21a68c5423e53c3c7149e12cc7bc0b1a4596fae4
Malaka98 <malaka.aruna14413@gmail.com>
2022-05-07 08:37:41 +0530

    feat

--- src/components/charts/pie-chart.component.tsx
+++ src/components/charts/pie-chart.component.tsx
@@ -1,52 +1,74 @@
-import React, {PureComponent} from 'react';
-import {PieChart, Pie, Sector, Cell, ResponsiveContainer} from 'recharts';
+import React from 'react';
+import {Cell, Pie, PieChart, ResponsiveContainer} from 'recharts';
+import {useGetQuery} from "../../service/resource-api.service";
+import {Center, Spinner} from "@chakra-ui/react";
 
-const data = [
-    {name: 'Projects', value: 400},
-    {name: 'Publications', value: 300},
-    {name: 'Patents', value: 300},
-    {name: 'IP Certificates', value: 200},
-];
+// const data = [
+//     {name: 'Projects', value: 400},
+//     {name: 'Publications', value: 300},
+//     {name: 'Patents', value: 300},
+//     {name: 'IP Certificates', value: 200},
+//     {name: 'IP Certificates2', value: 200},
+// ];
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const PieChartComponent = () => {
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}: any) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    const {isLoading, data} = useGetQuery({doc: "accident_type_percentage"})
+
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#513295'];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}: any) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {/*{`${(percent * 100).toFixed(0)}%`}*/}
+                {`${data[index].name} ${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    const spinnerMarkup = (
+        <Center>
+            <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl'
+            />
+        </Center>
+
+    );
 
     return (
-        <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {/*{`${(percent * 100).toFixed(0)}%`}*/}
-            {`${data[index].name} ${(percent * 100).toFixed(0)}%`}
-        </text>
-    );
-};
-
-export default class RichPieChart extends PureComponent {
-    static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-label-dlhhj';
-
-    render() {
-        return (
-            <ResponsiveContainer width="100%" height="100%">
-                <PieChart width={600} height={600}>
-                    <Pie
-                        data={data}
-                        cx="50%"
-                        cy="40%"
-                        labelLine={false}
-                        label={renderCustomizedLabel}
-                        outerRadius={80}
-                        fill="#8884d8"
-                        dataKey="value"
-                    >
-                        {data.map((entry, index) => (
-                            <Cell lang={entry.name} key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
-                        ))}
-                    </Pie>
-                </PieChart>
-            </ResponsiveContainer>
-        );
-    }
+        <>
+            {
+                isLoading ? spinnerMarkup :
+                    <ResponsiveContainer width="100%" height="100%">
+                        <PieChart width={600} height={600}>
+                            <Pie
+                                data={data}
+                                cx="50%"
+                                cy="40%"
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={100}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {data?.map((entry: any, index: number) => (
+                                    <Cell lang={entry.name} key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+                                ))}
+                            </Pie>
+                        </PieChart>
+                    </ResponsiveContainer>
+            }
+        </>
+    )
 }
+
+export default PieChartComponent
--- src/pages/dashboard/dashboard.page.tsx
+++ src/pages/dashboard/dashboard.page.tsx
@@ -1,14 +1,12 @@
-import {Box, Flex, Text, Heading, HStack, Progress, SimpleGrid, Stack} from '@chakra-ui/react'
+import {Box, Heading, HStack, SimpleGrid, Stack} from '@chakra-ui/react'
 import {StatCard} from "../../components/stat-card/stat-card.component";
-import {useDispatch} from "react-redux";
 // import {getReceivedApplications} from "../received-applications/actions/received-application-page.action";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 // import getPendingApplicationsAction from "../pending-applications/actions/pending-applications-page.actions";
 // import getApprovedApplicationsAction from "../approved-applications/actions/approved-applications-page.actions";
 // import getRejectedApplicationsAction from "../rejected-applications/actions/reject-applications-page.action";
 import SimpleLineChart from "../../components/charts/simple-line-chart.component";
 import RichPieChart from "../../components/charts/pie-chart.component";
-import RichRadarChart from "../../components/charts/radar-chart.component";
 import BarChartNoPadding from "../../components/charts/bar-chart-no-padding.component";
 
 const DashboardPage = () => {
@@ -31,51 +29,6 @@ const DashboardPage = () => {
     //     const rejectedApp = await dispatch(getRejectedApplicationsAction())
     //     setRejectedApp(rejectedApp.length)
     // }
-
-    const chartData = [
-        {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
 
     const data: any = [
         {
@@ -128,125 +81,11 @@ const DashboardPage = () => {
 
                 <Stack borderRadius={8} shadow={"md"} bg={"white"} p={2} pe={6} h={400}>
                     <HStack m={3}>
-                        <Heading size={"sm"}>Application Summery</Heading>
+                        <Heading size={"sm"}>Total Accident Percentage Summery</Heading>
                     </HStack>
                     <RichPieChart/>
                 </Stack>
             </SimpleGrid>
-
-            {/*<Flex direction="column" w="100%">*/}
-            {/*    <Flex*/}
-            {/*        direction="column"*/}
-            {/*        mt="24px"*/}
-            {/*        mb="10px"*/}
-            {/*        alignSelf="flex-start"*/}
-            {/*    >*/}
-            {/*        <Text*/}
-            {/*            fontSize="lg"*/}
-            {/*            color={"black"}*/}
-            {/*            fontWeight="bold"*/}
-            {/*            mb="6px"*/}
-            {/*        >*/}
-            {/*            Running Activities*/}
-            {/*        </Text>*/}
-            {/*        <Text fontSize="md" fontWeight="medium" color="gray.400">*/}
-            {/*            <Text as="span" color="green.400" fontWeight="bold">*/}
-            {/*                (+23%)*/}
-            {/*            </Text>{" "}*/}
-            {/*            than last week*/}
-            {/*        </Text>*/}
-            {/*    </Flex>*/}
-            {/*    <SimpleGrid gap={{sm: "12px"}} columns={4}>*/}
-            {/*        <Flex direction="column" bg={"white"} p={5} borderRadius={8} shadow={"md"}>*/}
-            {/*            <Flex alignItems="center">*/}
-            {/*                <Text fontSize="sm" color="gray.400" fontWeight="semibold">*/}
-            {/*                    Users*/}
-            {/*                </Text>*/}
-            {/*            </Flex>*/}
-            {/*            <Text*/}
-            {/*                fontSize="lg"*/}
-            {/*                color={"black"}*/}
-            {/*                fontWeight="bold"*/}
-            {/*                mb="6px"*/}
-            {/*                my="6px"*/}
-            {/*            >*/}
-            {/*                14582*/}
-            {/*            </Text>*/}
-            {/*            <Progress*/}
-            {/*                colorScheme="teal"*/}
-            {/*                borderRadius="12px"*/}
-            {/*                h="5px"*/}
-            {/*                value={92}*/}
-            {/*            />*/}
-            {/*        </Flex>*/}
-            {/*        <Flex direction="column" bg={"white"} p={5} borderRadius={8} shadow={"md"}>*/}
-            {/*            <Flex alignItems="center">*/}
-            {/*                <Text fontSize="sm" color="gray.400" fontWeight="semibold">*/}
-            {/*                    Collaboration*/}
-            {/*                </Text>*/}
-            {/*            </Flex>*/}
-            {/*            <Text*/}
-            {/*                fontSize="lg"*/}
-            {/*                color={"black"}*/}
-            {/*                fontWeight="bold"*/}
-            {/*                mb="6px"*/}
-            {/*                my="6px"*/}
-            {/*            >*/}
-            {/*                75*/}
-            {/*            </Text>*/}
-            {/*            <Progress*/}
-            {/*                colorScheme="teal"*/}
-            {/*                borderRadius="12px"*/}
-            {/*                h="5px"*/}
-            {/*                value={80}*/}
-            {/*            />*/}
-            {/*        </Flex>*/}
-            {/*        <Flex direction="column" bg={"white"} p={5} borderRadius={8} shadow={"md"}>*/}
-            {/*            <Flex alignItems="center">*/}
-            {/*                <Text fontSize="sm" color="gray.400" fontWeight="semibold">*/}
-            {/*                    Running Tasks*/}
-            {/*                </Text>*/}
-            {/*            </Flex>*/}
-            {/*            <Text*/}
-            {/*                fontSize="lg"*/}
-            {/*                color={"black"}*/}
-            {/*                fontWeight="bold"*/}
-            {/*                mb="6px"*/}
-            {/*                my="6px"*/}
-            {/*            >*/}
-            {/*                5004*/}
-            {/*            </Text>*/}
-            {/*            <Progress*/}
-            {/*                colorScheme="teal"*/}
-            {/*                borderRadius="12px"*/}
-            {/*                h="5px"*/}
-            {/*                value={65}*/}
-            {/*            />*/}
-            {/*        </Flex>*/}
-            {/*        <Flex direction="column" bg={"white"} p={5} borderRadius={8} shadow={"md"}>*/}
-            {/*            <Flex alignItems="center">*/}
-            {/*                <Text fontSize="sm" color="gray.400" fontWeight="semibold">*/}
-            {/*                    Funded Projects*/}
-            {/*                </Text>*/}
-            {/*            </Flex>*/}
-            {/*            <Text*/}
-            {/*                fontSize="lg"*/}
-            {/*                color={"black"}*/}
-            {/*                fontWeight="bold"*/}
-            {/*                mb="6px"*/}
-            {/*                my="6px"*/}
-            {/*            >*/}
-            {/*                100*/}
-            {/*            </Text>*/}
-            {/*            <Progress*/}
-            {/*                colorScheme="teal"*/}
-            {/*                borderRadius="12px"*/}
-            {/*                h="5px"*/}
-            {/*                value={20}*/}
-            {/*            />*/}
-            {/*        </Flex>*/}
-            {/*    </SimpleGrid>*/}
-            {/* </Flex>*/}
         </Box>
     )
 }
